5561291a7ffbf24f036c32f3115fa532a527852f
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-08-24 16:01:34 +0530

    minor changes

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -4,15 +4,13 @@ import styles from "./homePage.module.css";
 import { Link } from "react-router-dom";
 import { cardsArr } from "../../utils/avatar/cards";
 
-import one from "./../../assets/home/1.png";
-import two from "./../../assets/home/2.png";
-import three from "./../../assets/home/3.png";
-import four from "./../../assets/home/4.png";
-import five from "./../../assets/home/5.png";
-import six from "./../../assets/home/6.png";
+import one from "./../../assets/home/card01.jpeg";
+import two from "./../../assets/home/card02.jpeg";
+import three from "./../../assets/home/card03.jpeg";
+import four from "./../../assets/home/card04.jpeg";
 
 export default function HomePage() {
-  const imgsArr = [one, two, three, four, five, six];
+  const imgsArr = [one, two, three, four];
 
   return (
     <div className={`flex-col-center ${styles.HomePage}`}>
@@ -21,7 +19,7 @@ export default function HomePage() {
           <div
             key={idx}
             className={`flex-row-center ${styles.singleImg} ${
-              idx == 2 ? styles.exception : ""
+              idx == 2 ? "" : ""
             }`}
           >
             <img src={item} alt="avatar" />
